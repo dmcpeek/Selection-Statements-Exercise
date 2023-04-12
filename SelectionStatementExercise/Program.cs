2afdaf98d1fa4e9986efda3e08f9ad60e1f1c305
Doug McPeek <dmcpeek@outlook.com>
2023-04-12 10:56:09 -0600

    First checkin

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var randomNumber = new Random();
+            var favNumber = randomNumber.Next(1, 11);
+
+            Console.WriteLine("Try to guess my favorite number between 1 and 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Your guess is too low. My favorite number is {favNumber} and not {userInput}");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"Your guess is too high. My favorite number is {favNumber} and not {userInput}");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! My favorite number is {favNumber}");
+            }
         }
     }
 }
